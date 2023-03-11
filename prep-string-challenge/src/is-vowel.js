a9fec0f75e033abe7619aca6765978165e7d1299
Alex Arce <arcealejandrino@gmail.com>
2023-03-11 19:28:52 +0000

    complete string challenge

---
+++ prep-string-challenge/src/is-vowel.js
@@ -0,0 +1,13 @@
+/* exported isVowel */
+function isVowel(char) {
+  switch (char.toLowerCase()) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      return true;
+    default:
+      return false;
+  }
+}
